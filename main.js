8e842216ee42305dcbb3850c391a26076475abb4
Kerbery <108019017+Kerbery@users.noreply.github.com>
2022-06-22 23:39:47 +0300

    Added crawler

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+var Crawler = require("crawler");
+ 
+var c = new Crawler({
+    maxConnections : 10,
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            console.log($("#appHubAppName").text());
+        }
+        done();
+    }
+});
+
+c.queue('https://store.steampowered.com/app/70');
